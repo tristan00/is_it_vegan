f8eb4935149e1ac3629542ecc21e2afb637ffe02
Tristan Delforge <tristandelforge1@gmail.com>
2023-05-14 20:56:09 -0700

    init

---
+++ scrapers/menu_parser.py
@@ -0,0 +1,14 @@
+
+'''
+Given a menu url:
+
+Read menu
+Check if the whole menu is vegan or not. It should be indicated in the restaurant description.
+Identify vegan marker on menu for example v, vg, vegan...
+Collect dataset of each menu items, and wether they are vegan
+'''
+
+
+
+if __name__ == '__main__':
+    la_mar_url = 'https://www.lamarsf.com/la-mar/'
---
+++ scrapers/recipe_scraper.py
@@ -0,0 +1,6 @@
+'''
+Take input link
+Determine if link is a readable recipe
+If it is readable, scrape the title, ingredients, portions, instructions and pictures
+'''
+
